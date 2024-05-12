4e95cecf499f73611ef814f89a597145d724d8db
ANAND RAJ <anandraj93054@gmail.com>
2024-05-12 23:21:05 +0530

    Add files via upload

---
+++ src/components/theme/ToggleButton.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import classNames from 'classnames';
+
+const ToggleButton = ({ setTheme }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleMode = () => {
+    setIsDarkMode(!isDarkMode);
+    // You can add logic here to toggle between dark mode and light mode
+    setTheme(isDarkMode ? 'bg-gray-700 text-white' : 'bg-gray-300 text-black');
+  };
+
+  const buttonClasses = classNames(
+    'rounded-full',
+    'p-1',
+    'transition-colors',
+    'ml-12',
+    {
+      'bg-gray-300 dark:bg-gray-700': !isDarkMode,
+      'bg-gray-700 dark:bg-gray-300': isDarkMode,
+    }
+  );
+
+  return (
+    <button className={buttonClasses} onClick={toggleMode}>
+      {isDarkMode ? 'Light Mode' : 'Dark Mode'}
+    </button>
+  );
+};
+
+export default ToggleButton;
+
